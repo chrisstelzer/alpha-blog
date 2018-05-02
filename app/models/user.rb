dd21c4a48ea9800498d156ffeef3a886266adf40
Chris Stelzer <chris.j.stelzer@gmail.com>
2018-05-02 19:13:34 +0000

    added user validations with simple regx

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,10 @@
 class User < ApplicationRecord
-  
+  validates :username, presence: true, 
+            uniqueness: { case_sensitive: false }, 
+            length: { minimum: 3, maximum: 25 }
+  VALID_EMAIL_REGX = /@/
+  #should use email activation link so you can use simple reg ex above (https://davidcel.is/posts/stop-validating-email-addresses-with-regex/)
+  validates :email, presence: true, length: { maximum: 105 },
+            uniqueness: { case_sensitive: false },
+            format: { with: VALID_EMAIL_REGX }
 end
